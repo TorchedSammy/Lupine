0e118a8f6449d3ac9e48cabb6a0476d3fd88cdbe
sammy <38820196+TorchedSammy@users.noreply.github.com>
2021-04-25 15:38:45 -0400

    feat: add Loader function for lua module

--- rosettelog/rosettelog.go
+++ rosettelog/rosettelog.go
@@ -2,7 +2,21 @@ package rosettelog
 
 import (
 	"fmt"
+
+	"github.com/yuin/gopher-lua"
+	"layeh.com/gopher-luar"
 )
+
+func Loader(L *lua.LState) int {
+	mod := L.SetFuncs(L.NewTable(), map[string]lua.LGFunction{})
+	L.SetField(mod, "success", luar.New(L, Success))
+	L.SetField(mod, "fail", luar.New(L, Fail))
+	L.SetField(mod, "critical", luar.New(L, Critical))
+
+	L.Push(mod)
+
+	return 1
+}
 
 func Success(msg interface{}) {
 	Symbol('+', "2", msg)
