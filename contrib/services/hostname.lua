86d0df98b8df6083de7a3f0e1f83bcbe62a38bf1
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-10-11 10:15:36 -0400

    feat: remove hostname service, add tty service

--- contrib/services/hostname.lua
+++
@@ -1,4 +0,0 @@
-local lupinelog = require 'lupinelog'
-
-os.execute 'hostname -F /etc/hostname'
-lupinelog.success 'Set system hostname'
---
+++ contrib/services/tty.lua
@@ -0,0 +1,3 @@
+for i = 0, 8 do
+	os.execute('getty 38400 tty' .. i)
+end
